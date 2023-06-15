83b32aa9e600824240e62f478cb1d8d548fe42a3
ChoiWonSeo <cws4725@gmail.com>
2023-06-15 14:11:57 +0900

    mysql

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const path = require('path');
 
 require('dotenv').config({
@@ -13,8 +13,51 @@ const pool = mysql.createPool({
   database: process.env.DB_DB,
 });
 
-pool.query("SELECT * FROM player", (err, results) => {
-  for(const r of results) {
-    console.log(r);
-  }
-});
+// const insertData = {
+//   name: 'bengi',
+//   lane: 'jg',
+//   team: 't1',
+//   kills: 5,
+//   deaths: 3,
+//   assists: 8
+// };
+
+// pool.query("INSERT INTO player SET ?", insertData)
+//   .then(()=>{
+//     console.log('INSERT 성공')
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+const updateData = {
+  name: "sungwoong",
+  kills: 15,
+  deaths: 10
+}
+
+// pool.query("UPDATE player SET ? WHERE name = ?", [updateData, "bengi"])
+//   .then(() => {
+//     console.log("UPDATE 성공");
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+pool.query("DELETE FROM player WHERE name = ?", "sungwoong")
+  .then(() => {
+    console.log("DELETE 성공");
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+pool.query("SELECT * FROM player")
+  .then(([results]) => {
+    for(const r of results) {
+      console.log(r);
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
